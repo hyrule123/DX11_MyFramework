7505b9345575d45ceb4a6da352cacdd5a07d65df
hw <ekdrn1@naver.com>
2023-03-11 20:07:01 +0900

    줄바꿈 CRLF로 변경

--- Copy_StormLib_DLL.bat
+++ Copy_StormLib_DLL.bat
@@ -1,14 +1,16 @@
-:: *.h 파일 복사(라이브러리 참조용)
-xcopy /d /s /y /i /r /exclude:exclude_list_stormlib.txt ".\Project\StormLib_DLL\SRC\*.h" ".\External\Include\StormLib_DLL\"
+chcp 65001
 
-if exist ".\External\Library\StormLib_DLL\StormLib_DLL_Debug.dll" (
-xcopy /d /s /y /i ".\External\Library\StormLib_DLL\StormLib_DLL_Debug.dll" ".\OutputFile\Bin_Debug\"
+:: Copy *.h file(for library reference) 테스트
+xcopy /d /s /y /i /r /exclude:exclude_list_stormlib.txt "./Project/StormLib_DLL/SRC/*.h" "./External/Include/StormLib_DLL/"
+
+if exist "./External/Library/StormLib_DLL/StormLib_DLL_Debug.dll" (
+xcopy /d /s /y /i "./External/Library/StormLib_DLL/StormLib_DLL_Debug.dll" "./OutputFile/Bin_Debug/"
 )
 
-if exist ".\External\Library\StormLib_DLL\StormLib_DLL_Release.dll" (
-xcopy /d /s /y /i ".\External\Library\StormLib_DLL\StormLib_DLL_Release.dll" ".\OutputFile\Bin_Release\"
+if exist "./External/Library/StormLib_DLL/StormLib_DLL_Release.dll" (
+xcopy /d /s /y /i "./External/Library/StormLib_DLL/StormLib_DLL_Release.dll" "./OutputFile/Bin_Release/"
 )
 
 
-:: *.h 읽기 전용으로 변경
-attrib +r ".\External\Include\StormLib_DLL\*.h"
+:: Add read-only attributes to *.h files
+attrib +r "./External/Include/StormLib_DLL/*.h"
