7f7a0d59c9d6f249bbf3977982d6063a0b203f3c
HWNOTE <ekdrn1@naver.com>
2023-12-01 16:00:17 +0900

    define_SCMap 헤더 추가(맵 관련 변수 등록), EngineBase 업데이트

--- Project/UserContents/Com_Renderer_TilemapSC.h
+++ Project/UserContents/Com_Renderer_TilemapSC.h
@@ -1,6 +1,7 @@
 #pragma once
-
 #include <EngineBase/Engine/IComponent.h>
+
+#include "define_SCMap.h"
 
 enum class eTilemap_DebugMode
 {
@@ -9,9 +10,13 @@ enum class eTilemap_DebugMode
     MINITILE
 };
 
+
+
 namespace ehw
 {
+    class StructBuffer;
     class CS_TilemapLoader;
+    class Texture;
     class Com_Renderer_TilemapSC :
         public IComponent
     {
@@ -23,9 +28,33 @@ namespace ehw
         virtual void FixedUpdate() override {}
 
     private:
-        std::shared_ptr<CS_TilemapLoader> m_TilemapLoaderCS;
+        static int m_iRefCount_TilesetData;
+        static std::unique_ptr<StructBuffer[]> m_arrSBuffer_TilesetData[(int)SC_Map::eTILESET_INFO::END];
+
+        std::shared_ptr<CS_TilemapLoader> m_TilemapLoader;
+
+        uint m_uNumMegatileX;
+        uint m_uNumMegatileY;
+
+        SC_Map::eTILESET_INFO m_eTileSet;
+
+        std::string m_strMapName;
 
 
+        std::vector<tMegaTile> m_vecMegaTile;
+        std::vector<tMiniTile> m_vecMiniTile;
+        std::vector<SC_Map::tUnitData> m_vecUnitData;
+        std::vector<float2> m_vecStartLocation;
+
+        bool    m_bMapLoaded;
+        bool    m_bUnitLoaded;
+
+
+        //Buffers
+        std::shared_ptr<Texture> m_pMapTex;
+        std::shared_ptr<StructBuffer> m_pSBuffer_MXTM;
+        std::shared_ptr<StructBuffer> m_pSBufferRW_Megatile;
+        std::shared_ptr<StructBuffer> m_pSBufferRW_Minitile;
     };
 }
 
